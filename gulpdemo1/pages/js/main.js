91361d9681e0fee3064a459a40af72cdebe1c6b5
liyubiao <liyubiao@uniontech.com>
2021-08-13 16:28:04 +0800

    update

--- gulpdemo1/pages/js/main.js
+++ gulpdemo1/pages/js/main.js
@@ -1,5 +1,5 @@
+var layer1 = document.querySelector("#layer1");
 var sun = document.querySelector("#path96117");
-
 var deg = 0
 var round = null
 function rota() {
@@ -15,48 +15,34 @@ function rota() {
     }, 15)
 }
 rota()
+layer1.style.transform = mtrToCss(scale(.3))
 
-var layer2 = document.querySelector("#layer2");
+const layer2 = document.querySelector("#layer2");
 layer2.style.transform = 'translate(200px)'
-var drop = `<path  d="m48.243 163.98a39.741 206.89 0 0 1-48.243 0.98344l23.998-164.91z" fill="#00a1eb" fill-opacity=".91626" opacity=".9" stroke-width="1.331" />`
-// var drop = document.querySelector("#layer2 path");
+const drop = `<path  d="m48.243 163.98a39.741 206.89 0 0 1-48.243 0.98344l23.998-164.91z" fill="#00a1eb" fill-opacity=".91626" opacity=".9" stroke-width="1.331" />`
 
-// var h = 0
-// var d = null
-// function down(){
-//     if (d){
-//         clearTimeout(d)
-//         d = null
-//     }
-//     let size = 0.15
-//     let step = (Math.random()*5 + 3)/size
-//     h = h > 400/size ? -100 : h + step;
-//     d = setTimeout(function () {
-//         // console.log(h)
-//         drop.style.transform = mtrToCss(mtrMult(scale(size), translate(20,h)))
-//         down()
-//     }, 15)
-// }
-// down()
 let dropid = 0
 function createDrop(){
-    layer2.innerHTML += `<path id='dropid${++dropid}' d="m48.243 163.98a39.741 206.89 0 0 1-48.243 0.98344l23.998-164.91z" fill="#00a1eb" fill-opacity=".91626" opacity=".9" stroke-width="1.331" />`
-    const id = dropid
+    layer2.innerHTML += drop
+    const id = dropid++
     function animation(id){
         let h = 0;
         let timer = null;
         let size = Math.random()*0.1 + 0.05
-        let step = (Math.random()*5 + 3)/size
+        let step = (Math.random()*5 + 5)/size
         let x = Math.random()*200/size
         function down(){
-            const path = document.querySelector(`#dropid${id}`)
+            const path = document.querySelectorAll(`#layer2 path`)[id]
             if (timer){
                 clearTimeout(timer)
                 timer = null
             }
             if(h > 400/size){
+                size = Math.random()*0.1 + 0.05
+                step = (Math.random()*5 + 3)/size
                 h = -10/size
                 x = Math.random()*200/size
+                // return
             }
             h = h + step;
             timer = setTimeout(function () {
@@ -68,22 +54,8 @@ function createDrop(){
     }
     return animation(id)
 }
-createDrop()
-createDrop()
-createDrop()
-createDrop()
-createDrop()
-createDrop()
-createDrop()
-createDrop()
-createDrop()
-createDrop()
-createDrop()
-createDrop()
-createDrop()
-createDrop()
-createDrop()
-createDrop()
-createDrop()
-createDrop()
-createDrop()
+
+for (let i = 0; i < 20; i++) {
+    createDrop()
+}
+
