952d21c2cf87f83b13c6fa4773457910340aed7a
liyubiao <liyubiao@uniontech.com>
2021-08-13 12:38:15 +0800

    feat:完善transform函数

--- gulpdemo1/pages/api/transform.js
+++ gulpdemo1/pages/api/transform.js
@@ -3,9 +3,11 @@
  * 矩阵乘法
  * @param {Array} matrixA 
  * @param {Array} matrixB 
+ * @param {Array} ···
+ * @param {Array} matrixN
  * @returns 
  */
-function mtrMultMtr(matrixA, matrixB) {
+function mtrMult3d(matrixA, matrixB) {
     let res = [];
     let b = 0;
     for (let k = 0; k < 16; k++) {
@@ -19,7 +21,11 @@ function mtrMultMtr(matrixA, matrixB) {
         if (b == 4) b = 0;
         res.push(n)
     }
-    return res;
+    let rest = [].slice.call(arguments,2,arguments.length)
+    if(rest.length)
+        return mtrMult3d(res, ...rest)
+    else
+        return res;
 }
 
 /**
@@ -29,7 +35,7 @@ function mtrMultMtr(matrixA, matrixB) {
  * @param {Number} z default:x
  * @returns 
  */
-function scale(x, y=x, z=x) {
+function scale3d(x, y=x, z=x) {
     return [
         x, 0, 0, 0,
         0, y, 0, 0,
@@ -45,7 +51,7 @@ function scale(x, y=x, z=x) {
  * @param {Number} z default:x
  * @returns 
  */
-function translate(x, y=x, z=x) {
+function translate3d(x, y=x, z=x) {
     return [
         1, 0, 0, 0,
         0, 1, 0, 0,
@@ -61,9 +67,9 @@ function translate(x, y=x, z=x) {
  * @param {Number} z default:x
  * @returns 
  */
-function rotate(x, y=x, z=x) {
-    let mixMatrix =  mtrMultMtr( rotateX(x), rotateY(y));
-    return  mtrMultMtr( mixMatrix, rotateZ(z));
+function rotate3d(x, y=x, z=x) {
+    let mixMatrix =  mtrMult3d( rotateX3d(x), rotateY3d(y));
+    return  mtrMult3d( mixMatrix, rotateZ3d(z));
 }
 
 /**
@@ -71,7 +77,10 @@ function rotate(x, y=x, z=x) {
  * @param {Number} a 旋转角度
  * @returns 
  */
-function rotateX(a) {
+function rotateX3d(a) {
+    if (a && a.endsWith('deg')){
+        a = parseFloat(a)*Math.PI/180
+    }
     return [
         1, 0, 0, 0,
         0, Math.cos(a), -Math.sin(a), 0,
@@ -85,8 +94,10 @@ function rotateX(a) {
  * @param {Number} a 旋转角度
  * @returns 
  */
-function rotateY(a) {
-
+function rotateY3d(a) {
+    if (a && a.endsWith('deg')){
+        a = parseFloat(a)*Math.PI/180
+    }
     return [
         Math.cos(a), 0, Math.sin(a), 0,
         0, 1, 0, 0,
@@ -100,7 +111,10 @@ function rotateY(a) {
  * @param {Number} a 旋转角度
  * @returns 
  */
-function rotateZ(a) {
+function rotateZ3d(a) {
+    if (a && a.endsWith('deg')){
+        a = parseFloat(a)*Math.PI/180
+    }
     return [
         Math.cos(a), -Math.sin(a), 0, 0,
         Math.sin(a), Math.cos(a), 0, 0,
@@ -113,9 +127,123 @@ function rotateZ(a) {
  * @param {Array} array 
  * @returns {string} str
  */
-function matrixArrayToCssMatrix(array) {
+function mtrToCss3d(array) {
     return "matrix3d(" + array.join(',') + ")";
 }
 
+//** 2D */
 
-console.log(matrixArrayToCssMatrix(mtrMultMtr(mtrMultMtr(translate(-97, -97, 0), rotate(4.5,4.5,0)),translate(97, 97, 0))))
+/**
+ * 矩阵乘法
+ * @param {Array} matrixA 
+ * @param {Array} matrixB 
+ * @param {Array} ···
+ * @param {Array} matrixN
+ * @returns 
+ */
+ function mtrMult(matrixA, matrixB) {
+    let res = [];
+    let b = 0;
+    for (let k = 0; k < 9; k++) {
+        let a = Math.floor(k / 3);
+        let n = 0;
+
+        for (let j = 0; j < 9; j += 3) {
+            n += matrixA[a + j] * matrixB[b + j];
+        }
+        b++;
+        if (b == 3) b = 0;
+        res.push(n)
+    }
+    let rest = [].slice.call(arguments,2,arguments.length)
+    if(rest.length)
+        return mtrMult(res, ...rest)
+    else
+        return res;
+}
+
+/**
+ * 缩放矩阵计算
+ * @param {Number} x 缩放系数
+ * @param {Number} y default:x
+ * @returns 
+ */
+function scale(x, y=x) {
+    return [
+        x, 0, 0,
+        0, y, 0,
+        0, 0, 1
+    ];
+}
+
+/**
+ * 缩放矩阵计算
+ * @param {Number} x 平移距离
+ * @param {Number} y default:x
+ * @returns 
+ */
+function translate(x, y=x) {
+    return [
+        1, 0, 0,
+        0, 1, 0,
+        x, y, 1
+    ];
+}
+
+/**
+ * 输入角度计算旋转矩阵
+ * @param {Number} a 旋转角度
+ * @returns 
+ */
+function rotate(a) {
+    if (a && a.endsWith('deg')){
+        a = parseFloat(a)*Math.PI/180
+    }
+    return [
+        Math.cos(a), -Math.sin(a),0,
+        Math.sin(a), Math.cos(a), 0,
+        0, 0, 1
+    ];
+}
+
+/**
+ * X轴旋转矩阵
+ * @param {Number} a 旋转角度
+ * @returns 
+ */
+function rotateX(a) {
+    if (a && a.endsWith('deg')){
+        a = parseFloat(a)*Math.PI/180
+    }
+    return [
+        1, 0, 0,
+        0, Math.cos(a), -Math.sin(a),
+        0, Math.sin(a), Math.cos(a)
+    ];
+}
+
+/**
+ * Y轴旋转矩阵
+ * @param {Number} a 旋转角度
+ * @returns 
+ */
+function rotateY(a) {
+    if (a && a.endsWith('deg')){
+        a = parseFloat(a)*Math.PI/180
+    }
+    return [
+        Math.cos(a), 0, Math.sin(a), 
+        0, 1, 0, 
+        -Math.sin(a), 0, Math.cos(a), 
+    ];
+}
+
+/**
+ * 将矩阵数组转换为css矩阵字符串
+ * @param {Array} array 
+ * @returns {string} str
+ */
+function mtrToCss(array) {
+    array = [array[0], array[3], array[1], array[4], array[2], array[5]]
+    return "matrix(" + array.join(',') + ")";
+}
--- gulpdemo1/pages/js/main.js
+++ gulpdemo1/pages/js/main.js
@@ -1,15 +1,19 @@
-var obj = document.querySelector("#path96117");
-var length = obj.getTotalLength();
+var sun = document.querySelector("#path96117");
+
 var deg = 0
 var round = null
-function rota(){
-    if(round)
+function rota() {
+    if (round){
         clearTimeout(round)
-    deg = deg>360? 0: deg + 0.54;
-    round = setTimeout(function(){
-            var matrix = `matrix(1, ${deg}, 0,1,-100,-100)`;
-            obj.style.transform = `translate(100px, 100px) rotate(${deg}deg) translate(-100px, -100px)`
-            rota()
-        },15) 
+        round = null
+    }
+    deg = deg > 360 ? 0 : deg + 0.54;
+    round = setTimeout(function () {
+        sun.style.transformOrigin = '100px 100px';
+        sun.style.transform = mtrToCss(rotate(deg + 'deg'))
+        rota()
+    }, 15)
 }
 rota()
+
+var drop = `<path d="m48.243 163.98a39.741 206.89 0 0 1-48.243 0.98344l23.998-164.91z" fill="#00a1eb" fill-opacity=".91626" opacity=".9" stroke-width="1.331" />`
